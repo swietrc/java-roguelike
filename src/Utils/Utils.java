d350dd4fbefee36630f0cbf67bbc0fd6da49ef1d
WIETRICH SIMON <simon.wietrich@etu.unistra.fr>
2015-12-20 10:32:21 +0100

    Add utils package and class containing constants

--- src/Utils/Utils.java
+++ src/Utils/Utils.java
@@ -1,10 +1,8 @@
 package Utils;
 
-/**
- * Created by simon on 16/12/15.
- */
 public class Utils {
     public static final String VERSION = "0.1";
+    public static final int TEXTSIZE = 16;
 
     public static final int WIDTH = 640;
     public static final int HEIGHT = 480;
