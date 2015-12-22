41b5d5cc35b2b4546899667f23897d304f038a2f
Swietrich <simon.wietrich@gmail.com>
2015-12-22 16:05:27 +0100

    Add Getters and setters for Character

--- src/Model/Character.java
+++ src/Model/Character.java
@@ -2,34 +2,46 @@ package Model;
 
 import java.util.*;
 
+import static Model.Direction.*;
+
 /**
  * 
  */
 public abstract class Character extends Entity {
 
+    private int gold;
+    private int strength;
+
+    private int x;
+    private int y;
+
     /**
      * Default constructor
      */
     public Character() {
+        super();
     }
 
-    /**
-     * 
-     */
-    private int gold;
+    public void move(Direction d) {
+        switch (d) {
+            case NORTH:
 
-    /**
-     * 
-     */
-    private int strengh;
-
-
-
-    /**
-     * 
-     */
-    public void trigger() {
-        // TODO implement here
+        }
     }
 
+    public int getGold() {
+        return gold;
+    }
+
+    public void setGold(int gold) {
+        this.gold = gold;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
 }
